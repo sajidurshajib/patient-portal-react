8e4325dd46b7cc1d6b0fe2b5fc7f9ee3783417c1
sajidurshajib <sajidur.inbox@gmail.com>
2021-12-21 17:46:13 +0600

    created: Home/Services component created

---
+++ src/components/Home/Services/Services.js
@@ -0,0 +1,39 @@
+import { faHospital, faMedkit, faUserMd } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import classes from './Serices.module.css'
+
+const Services = () => {
+    return (
+        <div className={classes.Services}>
+            <div>
+                <p>
+                    <FontAwesomeIcon icon={faHospital} /> E-Chamber
+                </p>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium, voluptate? Lorem ipsum dolor
+                    sit amet.
+                </p>
+            </div>
+            <div>
+                <p>
+                    <FontAwesomeIcon icon={faMedkit} /> Medicine Delivery
+                </p>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium, voluptate? Lorem ipsum dolor
+                    sit amet.
+                </p>
+            </div>
+            <div>
+                <p>
+                    <FontAwesomeIcon icon={faUserMd} /> Telemedicine
+                </p>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium, voluptate? Lorem ipsum dolor
+                    sit amet.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Services
