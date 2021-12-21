39e6e0677c0302c911ba006cbf2509e5dde4ccbd
sajidurshajib <sajidur.inbox@gmail.com>
2021-12-21 17:42:22 +0600

    added: Hero component added

--- src/components/Home/Hero/Hero.js
+++ src/components/Home/Hero/Hero.js
@@ -1,16 +1,29 @@
 import { Link } from 'react-router-dom'
+import BG from '../../../assets/img/background.jpg'
+import HeroImg from '../../../assets/img/family_healthx.png'
 import classes from './Hero.module.css'
 
 const Hero = () => {
     return (
-        <div className={classes.Hero}>
-            <h2>
-                Your Trusted Digital <br />
-                <span>Healthcare</span> Partner
-            </h2>
-            <div className={classes.ButtonGroup}>
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
+        <div
+            className={classes.Hero}
+            style={{
+                background: `url(${BG})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+            }}>
+            <div className={classes.left}>
+                <h2>
+                    Your Trusted Digital <br />
+                    <span>Healthcare</span> Partner
+                </h2>
+                <div className={classes.ButtonGroup}>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
+                </div>
+            </div>
+            <div className={classes.right}>
+                <img src={HeroImg} alt="Family" />
             </div>
         </div>
     )
