bef297432954f9bd9816619ad7892d457922050e
sajidurshajib <sajidur.inbox@gmail.com>
2021-12-22 17:35:33 +0600

    completed: Register component

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,9 +1,29 @@
+import { faTimesCircle, faUserPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import classes from './Register.module.css'
 
-const Register = () => {
+const Register = ({ registerModal }) => {
     return (
         <div className={classes.Register}>
-            <h2>Register</h2>
+            <div className={classes.Wrapper}>
+                <span onClick={() => registerModal(false)}>
+                    <FontAwesomeIcon icon={faTimesCircle} />
+                </span>
+
+                <h2>
+                    <FontAwesomeIcon icon={faUserPlus} /> Register
+                </h2>
+                <form>
+                    <input type="text" placeholder="Username" />
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <input type="password" placeholder="Confirm Password" />
+                    <button>Register</button>
+                </form>
+                <p className={classes.LoginNow}>
+                    Have an account? <span>Login now</span>
+                </p>
+            </div>
         </div>
     )
 }
