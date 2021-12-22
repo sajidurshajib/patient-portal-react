8b979b62b8c9ce280693949cc907b9c5457afb16
sajidurshajib <sajidur.inbox@gmail.com>
2021-12-22 17:35:04 +0600

    completed: Login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,27 @@
+import { faSignInAlt, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import classes from './Login.module.css'
 
-const Login = () => {
+const Login = ({ loginModal }) => {
     return (
         <div className={classes.Login}>
-            <h2>Login</h2>
+            <div className={classes.Wrapper}>
+                <span onClick={() => loginModal(false)}>
+                    <FontAwesomeIcon icon={faTimesCircle} />
+                </span>
+
+                <h2>
+                    <FontAwesomeIcon icon={faSignInAlt} /> Login
+                </h2>
+                <form>
+                    <input type="text" placeholder="Username" />
+                    <input type="password" placeholder="Password" />
+                    <button>Login</button>
+                </form>
+                <p className={classes.RegisterNow}>
+                    New user? <span>Register now</span>
+                </p>
+            </div>
         </div>
     )
 }
