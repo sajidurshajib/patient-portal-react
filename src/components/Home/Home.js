1d9aee144d3de37b077708b537980a71b1c5a3ff
sajidurshajib <sajidur.inbox@gmail.com>
2021-12-22 17:33:35 +0600

    added: Modal added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,24 @@
+import { useState } from 'react'
+import Login from '../Login/Login'
+import TransparentNav from '../Nav/TransparentNav/TransparentNav'
+import Register from '../Register/Register'
 import Detail from './Detail/Detail'
 import Hero from './Hero/Hero'
 import classes from './Home.module.css'
 import Services from './Services/Services'
 
 const Home = () => {
+    const [loginModal, setLoginModal] = useState(false)
+    const [registerModal, setRegisterModal] = useState(false)
+
     return (
         <div className={classes.Home}>
-            <Hero />
+            <TransparentNav />
+
+            {loginModal ? <Login loginModal={setLoginModal} /> : null}
+            {registerModal ? <Register registerModal={setRegisterModal} /> : null}
+
+            <Hero loginModal={setLoginModal} registerModal={setRegisterModal} />
             <Services />
             <Detail />
         </div>
