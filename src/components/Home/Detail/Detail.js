02bd9e1248880e974f45121e5dd4480954bed501
sajidurshajib <sajidur.inbox@gmail.com>
2021-12-22 13:14:31 +0600

    component: Design completed

--- src/components/Home/Detail/Detail.js
+++ src/components/Home/Detail/Detail.js
@@ -1,5 +1,6 @@
-import desktop from '../../../assets/img/desktop.png'
-import mobile from '../../../assets/img/mobile.jpg'
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import desktop from '../../../assets/img/desktop-telemedicine.jpg'
 import classes from './Detail.module.css'
 
 const Detail = () => {
@@ -11,17 +12,30 @@ const Detail = () => {
                         background: `url(${desktop})`,
                         backgroundPosition: 'center',
                         backgroundSize: 'cover',
-                        width: '500px',
-                        height: '250px',
-                    }}></div>
-                <div
-                    style={{
-                        background: `url(${mobile})`,
-                        backgroundPosition: 'center',
-                        backgroundSize: 'cover',
+                        width: '100%',
+                        height: '300px',
                     }}></div>
             </div>
-            <div></div>
+            <div>
+                <h2>Your patient portal</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem repellat aliquam aut facere ullam
+                    quia mollitia iste dicta, velit doloribus? Labore rem id a obcaecati. Lorem ipsum dolor sit amet
+                    consectetur, adipisicing elit. Non facere nulla. Lorem ipsum dolor sit amet.
+                </p>
+                <h3>Your services</h3>
+                <ul>
+                    <li>
+                        <FontAwesomeIcon icon={faCheckCircle} /> Save your health data.
+                    </li>
+                    <li>
+                        <FontAwesomeIcon icon={faCheckCircle} /> Get your e-prescription.
+                    </li>
+                    <li>
+                        <FontAwesomeIcon icon={faCheckCircle} /> Buy medicine from online.
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
