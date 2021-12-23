60e5e5b20b293f970f3b3158119c52fb6e54f4cb
sajidurshajib <sajidur.inbox@gmail.com>
2021-12-23 13:32:56 +0600

    import: import profile component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Home from './Home/Home'
 import Login from './Login/Login'
+import Profile from './Profile/Profile'
 import Register from './Register/Register'
 
-export { Home, Login, Register }
+export { Home, Login, Register, Profile }
