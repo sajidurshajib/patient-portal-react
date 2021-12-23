5538adf3f1ce1876f494ed7242edc638e0fb0124
sajidurshajib <sajidur.inbox@gmail.com>
2021-12-23 13:25:20 +0600

    created: ProfilePage created

---
+++ src/pages/ProfilePage.js
@@ -0,0 +1,11 @@
+import { Profile } from '../components/index'
+
+const ProfilePage = () => {
+    return (
+        <div>
+            <Profile />
+        </div>
+    )
+}
+
+export default ProfilePage
