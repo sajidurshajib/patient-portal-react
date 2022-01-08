432e7c9493836fda56b223b95d706e87aa5a7637
Sajidur Rahman <sajidur.inbox@gmail.com>
2022-01-08 16:01:04 +0600

    added: Summery and Prescription component added

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,14 +1,19 @@
+import Prescriptions from './Prescriptions/Prescriptions'
 import classes from './Profile.module.css'
 import ProfileCard from './ProfileCard/ProfileCard'
+import Summery from './Summery/Summery'
 
 const Profile = () => {
     return (
         <div className={classes.Profile}>
-            <div className={classes.Wrapper}>
+            <div className={`${classes.Wrapper} container`}>
                 <div>
                     <ProfileCard />
                 </div>
-                <div></div>
+                <div>
+                    <Summery />
+                    <Prescriptions />
+                </div>
             </div>
         </div>
     )
