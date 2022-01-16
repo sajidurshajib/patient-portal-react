926b83fb692b843a02c7058f17be1ee9f9849ed1
Sajidur Rahman <sajidur.inbox@gmail.com>
2022-01-16 20:28:55 +0600

    added: Nav added

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,3 +1,5 @@
+import { TransparentNav } from '../Nav/Nav'
+import Information from './Information/Information'
 import Prescriptions from './Prescriptions/Prescriptions'
 import classes from './Profile.module.css'
 import ProfileCard from './ProfileCard/ProfileCard'
@@ -6,9 +8,11 @@ import Summery from './Summery/Summery'
 const Profile = () => {
     return (
         <div className={classes.Profile}>
+            <TransparentNav />
             <div className={`${classes.Wrapper} container`}>
                 <div>
                     <ProfileCard />
+                    <Information />
                 </div>
                 <div>
                     <Summery />
