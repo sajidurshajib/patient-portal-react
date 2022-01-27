34b05b63b7a9f9ee2c7914fd58d4ee42857ae981
Sajidur Rahman <sajidur.inbox@gmail.com>
2022-01-27 23:02:39 +0600

    added: Page component created

---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,11 @@
+import { Register } from '../components/index'
+
+const RegisterPage = () => {
+    return (
+        <div>
+            <Register />
+        </div>
+    )
+}
+
+export default RegisterPage
