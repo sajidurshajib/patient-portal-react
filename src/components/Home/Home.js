d80d97e772fe80fa0cee2fd57d5ef39663e61b5f
Sajidur Rahman <sajidur.inbox@gmail.com>
2022-02-05 10:22:48 +0600

    initial

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,15 @@
-import { useState } from 'react'
 import { TransparentNav } from '../Nav/Nav'
-import { Login, Register } from '../index'
 import Detail from './Detail/Detail'
 import Hero from './Hero/Hero'
 import classes from './Home.module.css'
 import Services from './Services/Services'
 
 const Home = () => {
-    const [loginModal, setLoginModal] = useState(false)
-    const [registerModal, setRegisterModal] = useState(false)
-
     return (
         <div className={classes.Home}>
             <TransparentNav />
 
-            {loginModal ? <Login loginModal={setLoginModal} /> : null}
-            {registerModal ? <Register registerModal={setRegisterModal} /> : null}
-
-            <Hero loginModal={setLoginModal} registerModal={setRegisterModal} />
+            <Hero />
             <Services />
             <Detail />
         </div>
