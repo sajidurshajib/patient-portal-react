3ae89e152599fcafa2778f26413a6385af588092
Sajidur Rahman <sajidur.inbox@gmail.com>
2022-02-05 10:15:03 +0600

    design: initial design completed

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,28 +1,44 @@
-import { faTimesCircle, faUserPlus } from '@fortawesome/free-solid-svg-icons'
+import { faRegistered } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import BG from '.././../assets/img/background.jpg'
 import classes from './Register.module.css'
 
-const Register = ({ registerModal }) => {
+const Register = () => {
     return (
-        <div className={classes.Register}>
+        <div
+            className={classes.Register}
+            style={{ background: `url(${BG})`, backgroundPosition: 'center', backgroundSize: 'cover' }}>
             <div className={classes.Wrapper}>
-                <span onClick={() => registerModal(false)}>
-                    <FontAwesomeIcon icon={faTimesCircle} />
-                </span>
-
                 <h2>
-                    <FontAwesomeIcon icon={faUserPlus} /> Register
+                    <FontAwesomeIcon icon={faRegistered} />
+                    Register
                 </h2>
-                <form>
-                    <input type="text" placeholder="Username" />
-                    <input type="email" placeholder="Email" />
+                <form action="">
+                    <input type="text" placeholder="Name" />
+                    <input type="text" placeholder="Email" />
+                    <input type="text" placeholder="Phone number [11 digit]" pattern="[0-9]{11}" />
                     <input type="password" placeholder="Password" />
-                    <input type="password" placeholder="Confirm Password" />
-                    <button>Register</button>
+
+                    <input type="text" placeholder="Address" />
+
+                    <div className={classes.sexWrapper}>
+                        <div>
+                            <select name="sex" id="sex">
+                                <option value="" disabled selected>
+                                    Sex
+                                </option>
+                                <option value="">Male</option>
+                                <option value="">Female</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label htmlFor="dob">Date of birth</label>
+                            <input type="date" id="dob" />
+                        </div>
+                    </div>
+
+                    <button>Login</button>
                 </form>
-                <p className={classes.LoginNow}>
-                    Have an account? <span>Login now</span>
-                </p>
             </div>
         </div>
     )
