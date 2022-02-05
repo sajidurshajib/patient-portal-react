f894aef4c55befbcd360800b40a563afaaac06a7
Sajidur Rahman <sajidur.inbox@gmail.com>
2022-02-05 10:20:20 +0600

    deisgn: initial design completed

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,26 +1,23 @@
-import { faSignInAlt, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import BG from '.././../assets/img/background.jpg'
 import classes from './Login.module.css'
 
-const Login = ({ loginModal }) => {
+const Login = () => {
     return (
-        <div className={classes.Login}>
+        <div
+            className={classes.Login}
+            style={{ background: `url(${BG})`, backgroundPosition: 'center', backgroundSize: 'cover' }}>
             <div className={classes.Wrapper}>
-                <span onClick={() => loginModal(false)}>
-                    <FontAwesomeIcon icon={faTimesCircle} />
-                </span>
-
                 <h2>
-                    <FontAwesomeIcon icon={faSignInAlt} /> Login
+                    <FontAwesomeIcon icon={faSignInAlt} />
+                    Login
                 </h2>
-                <form>
-                    <input type="text" placeholder="Username" />
+                <form action="">
+                    <input type="text" placeholder="Email" />
                     <input type="password" placeholder="Password" />
                     <button>Login</button>
                 </form>
-                <p className={classes.RegisterNow}>
-                    New user? <span>Register now</span>
-                </p>
             </div>
         </div>
     )
