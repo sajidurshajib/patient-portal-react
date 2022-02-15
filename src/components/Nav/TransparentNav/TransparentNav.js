02cd1b586c4202d9fbb44955a5731f82d8ca0848
sajidurshajib <sajidur.inbox@gmail.com>
2022-02-15 15:09:57 +0600

    added: context and logout

--- src/components/Nav/TransparentNav/TransparentNav.js
+++ src/components/Nav/TransparentNav/TransparentNav.js
@@ -1,7 +1,31 @@
+import { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
+import { Auth, UserInfo } from '../../../allContext'
 import classes from './TransparentNav.module.css'
 
 const TransparentNav = () => {
+    const { stateAuth, dispatchAuth } = useContext(Auth)
+    const { dispatchUser } = useContext(UserInfo)
+
+    const [logShow, setLogShow] = useState(false)
+
+    const history = useHistory()
+
+    const logout = (e) => {
+        e.preventDefault()
+        dispatchUser({ type: 'remove' })
+        dispatchAuth({ type: 'remove' })
+    }
+
+    useEffect(() => {
+        if (stateAuth.auth === true) {
+            setLogShow(true)
+        } else {
+            setLogShow(false)
+        }
+    }, [stateAuth, history])
+
     return (
         <div className={classes.TransparentNav}>
             <div className="container">
@@ -14,6 +38,13 @@ const TransparentNav = () => {
                     <li>
                         <Link to="/profile">HEALTHx</Link>
                     </li>
+                    {logShow === true ? (
+                        <li>
+                            <span onClick={(e) => logout(e)}>Logout</span>
+                        </li>
+                    ) : (
+                        ''
+                    )}
                 </ul>
             </div>
         </div>
