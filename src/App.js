cd9e573775a55cbfe15db60e0b484b4c759d0317
sajidurshajib <sajidur.inbox@gmail.com>
2022-02-15 15:06:40 +0600

    implememnt: protected route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
+import { useReducer } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Auth, UserInfo } from './allContext'
 import { HomePage, ProfilePage, RegisterPage, LoginPage } from './pages/index'
+import { authState, authReducer } from './reducer/authReducer'
+import { userState, userReducer } from './reducer/userReducer'
+import ProtectedRoute from './routes/ProtectedRoute'
 
 const App = () => {
+    const [stateAuth, dispatchAuth] = useReducer(authReducer, authState)
+    const [stateUser, dispatchUser] = useReducer(userReducer, userState)
+
     return (
         <div>
-            <Router>
-                <Switch>
-                    <Route path="/" exact={true} component={HomePage} />
-                    <Route path="/profile" component={ProfilePage} />
-                    <Route path="/register" component={RegisterPage} />
-                    <Route path="/login" component={LoginPage} />
-                </Switch>
-            </Router>
+            <Auth.Provider value={{ stateAuth, dispatchAuth }}>
+                <UserInfo.Provider value={{ stateUser, dispatchUser }}>
+                    {/* Auth and User context provider  */}
+                    <Router>
+                        <Switch>
+                            <Route path="/" exact={true} component={HomePage} />
+                            <ProtectedRoute path="/profile" component={ProfilePage} redirect="/" />
+                            <Route path="/register" component={RegisterPage} />
+                            <Route path="/login" component={LoginPage} />
+                        </Switch>
+                    </Router>
+                    {/* Auth and User context end */}
+                </UserInfo.Provider>
+            </Auth.Provider>
         </div>
     )
 }
