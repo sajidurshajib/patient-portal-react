62dc6a91bfb3c660158b17e6c680ee6bc8151af7
Jahid11186 <zahidhasansamim@gmail.com>
2022-04-12 14:41:36 +0600

    apiv1 changed in Pulse.js

--- src/components/Indicators/Pulse.js
+++ src/components/Indicators/Pulse.js
@@ -11,7 +11,7 @@ const Pulse = () => {
     const [pulse, setPulse] = useState(0)
     const [dataPulse, setDataPulse] = useState([])
 
-    const apiV1 = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_V1 : env.REACT_APP_API_V1
+    const apiV1 = process.env.REACT_APP_API_V1
 
     let token = stateAuth.token
 
