633bc5a81692f1427619b2155dc01bca8c469bb1
sajidurshajib <sajidur.inbox@gmail.com>
2022-04-12 10:09:02 +0600

    added: Indicators

--- src/components/Indicators/index.js
+++ src/components/Indicators/index.js
@@ -1,9 +1,10 @@
 import Bp from './Bp'
-import DoubleNumber from './DoubleNumber/DoubleNumber'
-import Number from './Number/Number'
+import DoubleNumber from './Field/DoubleNumber/DoubleNumber'
+import Number from './Field/Number/Number'
+import Text from './Field/Text/Text'
+import Indicators from './Indicators'
 import PersonalHabits from './PersonalHabit/PersonalHabits'
 import Pulse from './Pulse'
 import Rbs from './Rbs'
-import Text from './Text/Text'
 
-export { Text, Number, PersonalHabits, Pulse, Rbs, DoubleNumber, Bp }
+export { Indicators, Text, Number, PersonalHabits, Pulse, Rbs, DoubleNumber, Bp }
