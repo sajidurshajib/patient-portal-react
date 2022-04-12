7a001ad645fcfe39b0aa3a2207c83d00f40031a5
sajidurshajib <sajidur.inbox@gmail.com>
2022-04-12 10:22:40 +0600

    added: Indicators

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import FindDoctorsPage from './FindDoctorsPage'
 import HomePage from './HomePage'
+import IndicatorsPage from './IndicatorsPage'
 import LoginPage from './LoginPage'
 import MedicalPage from './MedicalPage'
 import ProfilePage from './ProfilePage'
 import RegisterPage from './RegisterPage'
 import SettingsPage from './SettingsPage'
 
-export { HomePage, ProfilePage, FindDoctorsPage, RegisterPage, MedicalPage, LoginPage, SettingsPage }
+export { HomePage, ProfilePage, FindDoctorsPage, RegisterPage, MedicalPage, LoginPage, IndicatorsPage, SettingsPage }
