9463f8c2e89ae90119172eb5782565b761d5f98f
Jahid11186 <zahidhasansamim@gmail.com>
2022-05-26 18:11:08 +0600

    API fetched: for Profile picture

--- src/components/Profile/ProfileCard/ProfileCard.js
+++ src/components/Profile/ProfileCard/ProfileCard.js
@@ -1,18 +1,45 @@
-import { useContext } from 'react'
-import { UserInfo } from '../../../allContext'
+import { useContext, useEffect, useState } from 'react'
+import { Auth, UserInfo } from '../../../allContext'
 import proPic from '../../../assets/img/patient1.jpeg'
 import classes from './ProfileCard.module.css'
 import ProfileImgUpload from './ProfileImgUpload/ProfileImgUpload'
 
 const ProfileCard = ({ userDetail }) => {
     const { stateUser } = useContext(UserInfo)
+    const { stateAuth } = useContext(Auth)
+
+    const [pic, setPic] = useState()
+
+    const apiV1 = process.env.REACT_APP_API_V1
+    const token = stateAuth.token
+
+    useEffect(() => {
+        let imgInfoFunc = async () => {
+            let imgInfo = await fetch(`${apiV1}/profile-pic`, {
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+                method: 'GET',
+            })
+
+            let infoJson = await imgInfo.json()
+
+            if (imgInfo.ok) {
+                setPic(infoJson.image_string)
+            }
+        }
+    }, [apiV1, token])
+
+    const picUrl = 'http://127.0.0.1:8000/files/' + pic
 
     return (
         <div className={classes.ProfileCard}>
             <div
                 className={classes.PP}
                 style={{
-                    background: `url(${proPic})`,
+                    background: `url(${picUrl})`,
                     backgroundPosition: 'center',
                     backgroundSize: 'cover',
                     borderRadius: '10px',
