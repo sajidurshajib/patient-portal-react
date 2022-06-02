2c55f40b5972b6904a7971bad8067c2781bae2ba
Jahid11186 <zahidhasansamim@gmail.com>
2022-06-02 16:59:29 +0600

    Fetch API: Family member

--- src/components/Profile/Family/Family.js
+++ src/components/Profile/Family/Family.js
@@ -1,3 +1,5 @@
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState, useEffect, useContext } from 'react'
 import { Auth } from '../../../allContext'
 import classes from './Family.module.css'
@@ -10,13 +12,23 @@ const Family = () => {
 
     const [search, setSearch] = useState('')
     const [searchResult, setSearchResult] = useState('')
-    const [familyMember, setFamilyMember] = useState([])
+    const [user, setUser] = useState([])
     const [hide, setHide] = useState(false)
+
+    const [reqPopup, setReqPopup] = useState(false)
+    const [relationWith, setRelationWith] = useState()
+    const [relationFrom, setRelationFrom] = useState()
+    const [relationTo, setRelationTo] = useState()
+    const [message, setMessage] = useState('hello')
+
+    const popup = () => {
+        setReqPopup(!reqPopup)
+    }
 
     const handler = (search) => {
         let matches = []
-        if (search.length > 1) {
-            matches = familyMember[1].filter((familyMember) => familyMember.phone.toLowerCase().includes(search))
+        if (search.length > 10) {
+            matches = user[1].filter((user) => user.phone.toLowerCase().includes(search))
             setHide(true)
         }
         if (search.length < 11) {
@@ -28,19 +40,13 @@ const Family = () => {
 
     const setHandle = (search) => {
         setSearch(search)
-        setFamilyMember(search?.id)
+        setUser(search?.id)
         setSearchResult([])
         setHide(false)
     }
 
-    // const sendReq = async () => {
-    //     try{
-    //         const sent = await fetch()
-    //     }
-    // }
-
     useEffect(() => {
-        const fetchFamilyMember = async () => {
+        const fetchFamilyUser = async () => {
             try {
                 const response = await fetch(`${apiV1}/admin/patient/all?phone_number=${search}&skip=0&limit=11`, {
                     method: 'GET',
@@ -51,14 +57,37 @@ const Family = () => {
                     },
                 })
                 const fm = await response.json()
-                console.log(fm)
-                setFamilyMember(fm)
+                setUser(fm)
             } catch {
-                setFamilyMember([])
+                setUser([])
             }
         }
-        return fetchFamilyMember()
+        return fetchFamilyUser()
     }, [token, apiV1, search])
+
+    const familyReq = async (e) => {
+        e.preventDefault()
+        const reqFetch = await fetch(`${apiV1}/patient/family/`, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                relationWith,
+                relationFrom,
+                relationTo,
+                message,
+            }),
+        })
+        console.log(relationWith, relationTo, relationFrom, message)
+        if (reqFetch.ok) {
+            console.log('submitted')
+        } else {
+            console.log('error')
+        }
+    }
 
     return (
         <div className={classes.Family}>
@@ -72,8 +101,9 @@ const Family = () => {
                         onChange={(e) => handler(e.target.value)}
                     />
                 </form>
-
-                {hide && (
+            </div>
+            <div className={classes.list}>
+                {/* {hide && (
                     <div className={classes.optAll}>
                         {searchResult &&
                             searchResult.map((info, i) => (
@@ -86,12 +116,74 @@ const Family = () => {
                                 </div>
                             ))}
                     </div>
+                )} */}
+                {hide && (
+                    <div className={classes.optAll}>
+                        {searchResult &&
+                            searchResult.map((info, i) => (
+                                <div className={classes.optSelect} key={i}>
+                                    <div>
+                                        {info.name}-{info.id}
+                                        {/* <button onClick={() => setHandle(info)}>
+                                            <FontAwesomeIcon icon={faUserPlus} />
+                                        </button> */}
+                                        <button onClick={popup}>
+                                            <FontAwesomeIcon icon={faUserPlus} />
+                                        </button>
+                                    </div>
+
+                                    {reqPopup && (
+                                        <div className={classes.formPopup}>
+                                            <div onClick={reqPopup}></div>
+                                            <div className={classes.reqInfo}>
+                                                <h2>Send Request to {info.name}</h2>
+                                                <div className={classes.Content}>
+                                                    <form onSubmit={familyReq}>
+                                                        <label htmlFor="relationWith">Relation With</label>
+                                                        <input
+                                                            id="relationWith"
+                                                            type="number"
+                                                            value={relationWith}
+                                                            onChange={(e) => setRelationWith(e.target.value)}
+                                                        />
+
+                                                        <label htmlFor="relation_from">Relation From</label>
+                                                        <input
+                                                            id="relation_from"
+                                                            type="text"
+                                                            value={relationFrom}
+                                                            onChange={(e) => setRelationFrom(e.target.value)}
+                                                        />
+
+                                                        <label htmlFor="relation_to">Relation To</label>
+                                                        <input
+                                                            id="relation_to"
+                                                            type="text"
+                                                            value={relationTo}
+                                                            onChange={(e) => setRelationTo(e.target.value)}
+                                                        />
+
+                                                        <label htmlFor="relation_to">Message</label>
+                                                        <input
+                                                            id="message"
+                                                            type="text"
+                                                            value={message}
+                                                            onChange={(e) => setMessage(e.target.value)}
+                                                        />
+
+                                                        <button>Send request</button>
+                                                        <button onClick={popup}>Cancel</button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+                            ))}
+                    </div>
                 )}
             </div>
-            <div div className={classes.Content}>
-                <Members />
-                <Members />
-                <Members />
+            <div className={classes.Content}>
                 <Members />
             </div>
         </div>
--- src/components/Profile/Family/Members/Members.js
+++ src/components/Profile/Family/Members/Members.js
@@ -1,10 +1,50 @@
+import { useState, useEffect, useContext } from 'react'
+import { Auth } from '../../../../allContext'
 import classes from './Members.module.css'
 
 const Members = () => {
+    const { stateAuth } = useContext(Auth)
+    const token = stateAuth.token
+    const apiV1 = process.env.REACT_APP_API_V1
+
+    const [familyMember, setFamilyMember] = useState({})
+
+    useEffect(() => {
+        const familyFetch = async () => {
+            try {
+                const response = await fetch(`${apiV1}/patient/family/`, {
+                    method: 'GET',
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${token}`,
+                    },
+                })
+                const fm = await response.json()
+                console.log('Family member', fm)
+
+                if (response.ok) {
+                    setFamilyMember(fm)
+                }
+            } catch {
+                setFamilyMember({})
+            }
+        }
+        familyFetch()
+    }, [token, apiV1])
+
+    let data = Array.from(familyMember)
+
     return (
-        <div className={classes.Member}>
-            <h3>Hasibul Kabir</h3>
-            <p>Brother</p>
+        <div>
+            {data.map((member, i) => {
+                return (
+                    <div className={classes.Member} key={i}>
+                        <h3>{member.relation_with_name}</h3>
+                        <p>{member.relationship_status}</p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
