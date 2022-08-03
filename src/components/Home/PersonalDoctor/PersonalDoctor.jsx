8adcc42f17002ad51aa1c7417f48de22ff5c0224
morshedul-antor <antor.morshedul@gmail.com>
2022-08-03 10:44:42 +0600

    feat: connect to single doctor page

--- src/components/Home/PersonalDoctor/PersonalDoctor.jsx
+++ src/components/Home/PersonalDoctor/PersonalDoctor.jsx
@@ -1,6 +1,7 @@
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Doctor from '../../../assets/img/doc/docstock.jpg'
 import classes from './PersonalDoctor.module.css'
 
@@ -8,18 +9,21 @@ export default function PersonalDoctor() {
     return (
         <div className={classes.container}>
             <p>Your Family Doctor</p>
-            <div className={classes.flexWrapper}>
-                <div>
-                    <img src={Doctor} alt="" />
-                    <p>
-                        5.0 <FontAwesomeIcon icon={faStar} style={{ color: 'orange', fontSize: '8px' }} />
-                    </p>
+            <Link to={`/doctor/1002`}>
+                <div className={classes.flexWrapper}>
+                    <div>
+                        <img src={Doctor} alt="" />
+                        <p>
+                            5.0 <FontAwesomeIcon icon={faStar} style={{ color: 'orange', fontSize: '8px' }} />
+                        </p>
+                    </div>
+
+                    <div>
+                        <p>Dr. Rashadul Hasan</p>
+                        <span>5+ years experience</span>
+                    </div>
                 </div>
-                <div>
-                    <p>Dr. Rashadul Hasan</p>
-                    <span>5+ years experience</span>
-                </div>
-            </div>
+            </Link>
         </div>
     )
 }
