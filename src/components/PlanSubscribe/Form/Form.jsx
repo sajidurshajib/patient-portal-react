1a00d831beb39ac991f40d5898b705b6081d4d87
shariard58 <shariard58@gmail.com>
2022-09-21 12:30:50 +0600

    Form design last update : almost acceptable form

--- src/components/PlanSubscribe/Form/Form.jsx
+++ src/components/PlanSubscribe/Form/Form.jsx
@@ -1,145 +1,141 @@
 // import Image from 'next/image'
 import React, { useState } from 'react'
+import BG from '../../../assets/img/background1.png'
 // import BG from '../../../assets/img/background.jpg'
-import BG from '../../../assets/img/background.jpg'
+// import BG from '../../../assets/img/background.jpg'
 import FBG2 from '../../../assets/img/background_flower.jpg'
+import Comment from '../../../assets/img/comment.png'
 import FBG from '../../../assets/img/dark-blue-banner.jpg'
 import DarkBlueBG from '../../../assets/img/dark_blue_background.jpg'
 import FBG1 from '../../../assets/img/flowerBackground.jpg'
 import HealthPackage from '../../../assets/img/health_package9.jpg'
+import Member from '../../../assets/img/male.png'
+import Mobile from '../../../assets/img/mobile-payment.png'
+import Taka from '../../../assets/img/taka.png'
+import Time from '../../../assets/img/time.png'
 import classes from './Form.module.css'
 
 export default function Form({ plans, singlePlan, setPlanId }) {
     // const [planGet, setPlanGet] = useState({})
+    //this from  projet folder
 
     return (
-        <div className={classes.container} style={{}}>
-            <div className={classes.heading}>{/* <h2> Select your health Plan</h2> */}</div>
+        <div className={classes.container}>
             <div className={classes.wrapper}>
-                <div>
-                    <p>Select Your Health Plan</p>
+                <div
+                    style={
+                        {
+                            // background: `url(${BG})`,
+                            // backgroundPosition: "center",
+                            // backgroundSize: "cover",
+                            // height: "80vh",
+                            // backgroundRepeat: "no-repeat",
+                            // height: "100vh",
+                        }
+                    }>
+                    <h2> Get Your best health plan , here</h2>
+                    {/* img */}
+                    <img src={BG} />
                 </div>
 
-                <div className={classes.formSection}>
-                    <div
-                        style={{
-                            background: `url(${HealthPackage})`,
-
-                            backgroundPosition: 'center',
-                            backgroundSize: 'cover',
-                            height: '80vh',
-                            backgroundRepeat: 'no-repeat',
-                        }}>
-                        <p>Get your best health plan , here</p>
-                        <div> </div>
-                    </div>
+                <div>
+                    {/* first div */}
                     <div>
-                        <form className={classes.form} method="GET">
+                        <div>
+                            <label>Select Your Plan</label>
+                        </div>
+
+                        <div>
+                            {/* Mapping and On change  */}
+                            <select className={classes.label1} onChange={(e) => setPlanId(parseInt(e.target.value))}>
+                                <option value="">Select Plan</option>
+                                {plans &&
+                                    plans.map((plan) => (
+                                        <option value={plan.id}>
+                                            {plan.name} Basic - {plan.days / 30}M
+                                        </option>
+                                    ))}
+                            </select>
+                        </div>
+                    </div>
+                    {/* Second  div */}
+
+                    <div>
+                        {/* ====== Plan Duration ========= */}
+                        <div>
                             <div>
-                                <div>
-                                    <div>
-                                        <label>Select Plan</label>
-                                    </div>
+                                <label htmlFor="planDescription">Plan Duration </label>
+                            </div>
 
-                                    <div>
-                                        {/* Mapping and On change  */}
-                                        <select
-                                            className={classes.label1}
-                                            onChange={(e) => setPlanId(parseInt(e.target.value))}>
-                                            <option value="">Select Plan</option>
-                                            {plans &&
-                                                plans.map((plan) => (
-                                                    <option value={plan.id}>
-                                                        {plan.name} Basic - {plan.days / 30}M
-                                                    </option>
-                                                ))}
-                                        </select>
-                                    </div>
+                            <div className={classes.input}>
+                                <label className={classes.txtFeild}> {singlePlan.days}</label>
+                                <div>
+                                    <img className={classes.image1} src={Time} alt="" />{' '}
                                 </div>
                             </div>
+                        </div>
+
+                        {/* Plan Price  */}
+
+                        <div>
                             <div>
-                                {/* ======== for first div ========= */}
-                                <div>
-                                    {/* ====== Plan Duration ========= */}
-                                    <div>
-                                        <div>
-                                            <label htmlFor="planDescription">Plan Duration </label>
-                                        </div>
-
-                                        <div className={classes.input}>
-                                            <label className={classes.txtFeild}>{singlePlan.days}</label>
-                                        </div>
-                                    </div>
-
-                                    {/* Plan Price  */}
-
-                                    <div>
-                                        <div>
-                                            <label htmlFor="planPrice">Plan Price </label>
-                                        </div>
-
-                                        <div className={classes.middle}>
-                                            <label className={classes.txtFeild}>{singlePlan.fee}</label>
-                                        </div>
-                                    </div>
-
-                                    <div>
-                                        <div>
-                                            <label htmlFor="totalPatient">Total Patient</label>
-                                        </div>
-
-                                        <div className={classes.input}>
-                                            <label className={classes.txtFeild}>{singlePlan.total_patients}</label>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                {/* For the second div  */}
-
-                                <div></div>
-
-                                <div>
-                                    {/* ====== Select Palyment Method  ========= */}
-
-                                    <div>
-                                        <div>
-                                            <label htmlFor="payment"> Select Payment Method</label>
-                                        </div>
-
-                                        <div>
-                                            <select name="payment" id="payment">
-                                                <option value="bkash"> bkash</option>
-                                                <option value="rocket">rocket</option>
-                                                <option value="nagad">nagad</option>
-                                            </select>
-                                        </div>
-                                    </div>
-
-                                    {/* ====== Any Question========= */}
-
-                                    <div></div>
-
-                                    <div></div>
-                                </div>
-                                <div>
-                                    <div>
-                                        <label htmlFor="question"> Remarks</label>
-                                    </div>
-
-                                    <div>
-                                        <textarea name="question" id="question"></textarea>
-                                    </div>
-                                </div>
+                                <label htmlFor="planPrice">Plan Price </label>
                             </div>
 
-                            {/* submit button  */}
-
-                            <div className={classes.last}>
-                                <button className={classes.button} type="submit">
-                                    Submit
-                                </button>
+                            <div className={classes.input1}>
+                                <label className={classes.txtFeild}> {singlePlan.fee}</label>
+                                <img className={classes.image} src={Taka} alt="" />{' '}
                             </div>
-                        </form>
+                        </div>
+
+                        <div>
+                            <div>
+                                <label htmlFor="totalPatient">Total Patient</label>
+                            </div>
+
+                            <div className={classes.input2}>
+                                <label className={classes.txtFeild}> {singlePlan.total_patients}</label>
+
+                                <img className={classes.image} src={Member} alt="" />
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* third div  */}
+
+                    <div>
+                        <div>
+                            <label htmlFor="payment"> Select Payment Method</label>
+                        </div>
+                        <div>
+                            <select name="payment" id="payment">
+                                <option value="bkash"> bkash</option>
+                                <option value="rocket">rocket</option>
+                                <option value="nagad">nagad</option>
+                            </select>
+                            <img src={Mobile} alt="" />
+                        </div>{' '}
+                    </div>
+
+                    {/* forth div  */}
+
+                    <div>
+                        <div>
+                            <label htmlFor="question"> Remarks</label>
+                        </div>
+
+                        <div>
+                            <textarea name="question" id="question"></textarea>
+
+                            <img src={Comment} alt="" />
+                        </div>
+                    </div>
+
+                    {/* fifth div  */}
+                    <div className={classes.last}>
+                        <button className={classes.button} type="submit">
+                            Submit
+                        </button>
                     </div>
                 </div>
             </div>
