e4c674efce67c0272a05b9a958de272f486d4689
shariard58 <shariard58@gmail.com>
2022-09-21 06:26:53 +0600

    till first api fetching : first time api fetching for information

--- src/components/PlanSubscribe/Form/Form.jsx
+++ src/components/PlanSubscribe/Form/Form.jsx
@@ -1,26 +1,19 @@
 // import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 // import BG from '../../../assets/img/background.jpg'
 import BG from '../../../assets/img/background.jpg'
 import FBG2 from '../../../assets/img/background_flower.jpg'
 import FBG from '../../../assets/img/dark-blue-banner.jpg'
 import DarkBlueBG from '../../../assets/img/dark_blue_background.jpg'
-import HealthPackage from '../../../assets/img/family_health_plan.png'
 import FBG1 from '../../../assets/img/flowerBackground.jpg'
+import HealthPackage from '../../../assets/img/health_package9.jpg'
 import classes from './Form.module.css'
 
-export default function Form() {
+export default function Form({ plans, singlePlan, setPlanId }) {
+    // const [planGet, setPlanGet] = useState({})
+
     return (
-        <div
-            className={classes.container}
-            style={
-                {
-                    // background: `url(${BG})`,
-                    // backgroundPosition: 'center',
-                    // backgroundSize: 'cover',
-                    // height: '100vh',
-                }
-            }>
+        <div className={classes.container} style={{}}>
             <div className={classes.heading}>{/* <h2> Select your health Plan</h2> */}</div>
             <div className={classes.wrapper}>
                 <div>
@@ -30,29 +23,36 @@ export default function Form() {
                 <div className={classes.formSection}>
                     <div
                         style={{
-                            background: `url(${FBG})`,
-                            backgroundRepeat: 'no-repeat',
+                            background: `url(${HealthPackage})`,
+
                             backgroundPosition: 'center',
                             backgroundSize: 'cover',
-                            height: '100vh',
+                            height: '80vh',
+                            backgroundRepeat: 'no-repeat',
                         }}>
                         <p>Get your best health plan , here</p>
-                        <img src={HealthPackage} alt="" />
+                        <div> </div>
                     </div>
                     <div>
                         <form className={classes.form} method="GET">
                             <div>
                                 <div>
                                     <div>
-                                        <label for="payment">Select Plan</label>
+                                        <label>Select Plan</label>
                                     </div>
 
                                     <div>
-                                        <select className={classes.label1} name="payment" id="payment">
-                                            <option value="bkash"> My Health Plan (MHP) -Basic 1M </option>
-                                            <option value="rocket"> My Health Plan (MHP) -Basic 3M </option>
-                                            <option value="nagad"> My Health Plan (MHP) -Basic 6M </option>
-                                            <option value="nagad"> My Health Plan (MHP) -Basic 12M </option>
+                                        {/* Mapping and On change  */}
+                                        <select
+                                            className={classes.label1}
+                                            onChange={(e) => setPlanId(parseInt(e.target.value))}>
+                                            <option value="">Select Plan</option>
+                                            {plans &&
+                                                plans.map((plan) => (
+                                                    <option value={plan.id}>
+                                                        {plan.name} Basic - {plan.days / 30}M
+                                                    </option>
+                                                ))}
                                         </select>
                                     </div>
                                 </div>
@@ -60,33 +60,14 @@ export default function Form() {
                             <div>
                                 {/* ======== for first div ========= */}
                                 <div>
-                                    {/* ====== for Name ========= */}
-                                    {/* <div>
-                                        <div>
-                                            <label for="name">Plan Name</label>
-                                        </div>
-
-                                        <div className={classes.input}>
-                                            <input type="text" name="name" id="name" placeholder="name" required />{' '}
-                                            <label className={classes.txtFeild}></label>
-                                        </div>
-                                    </div> */}
-
                                     {/* ====== Plan Duration ========= */}
                                     <div>
                                         <div>
-                                            <label for="planDescription">Plan Duration </label>
+                                            <label htmlFor="planDescription">Plan Duration </label>
                                         </div>
 
                                         <div className={classes.input}>
-                                            {/* <input
-                                            type="text"
-                                            name="planDescription"
-                                            id="planDescription"
-                                            placeholder=""
-                                            required
-                                        />{' '} */}
-                                            <label className={classes.txtFeild}></label>
+                                            <label className={classes.txtFeild}>{singlePlan.days}</label>
                                         </div>
                                     </div>
 
@@ -94,87 +75,35 @@ export default function Form() {
 
                                     <div>
                                         <div>
-                                            <label for="planPrice">Plan Price </label>
+                                            <label htmlFor="planPrice">Plan Price </label>
                                         </div>
 
                                         <div className={classes.middle}>
-                                            {/* <input type="number" name="planPrice" id="planPrice" /> */}
-                                            <label className={classes.txtFeild}></label>
+                                            <label className={classes.txtFeild}>{singlePlan.fee}</label>
                                         </div>
                                     </div>
 
                                     <div>
                                         <div>
-                                            <label for="totalPatient">Total Patient</label>
+                                            <label htmlFor="totalPatient">Total Patient</label>
                                         </div>
 
-                                        {/* <div>
-                                            <input type="number" name="totalPatient" id="age" min="1" max="200" />
-                                        </div> */}
                                         <div className={classes.input}>
-                                            {/* <input type="number" name="planPrice" id="planPrice" /> */}
-                                            <label className={classes.txtFeild}></label>
+                                            <label className={classes.txtFeild}>{singlePlan.total_patients}</label>
                                         </div>
                                     </div>
                                 </div>
 
                                 {/* For the second div  */}
 
-                                <div>
-                                    {/* ====== for Phone ========= */}
-                                    {/* <div>
-                                        <div>
-                                            <label for="phone">Phone</label>
-                                        </div>
-
-                                        <div>
-                                            <input
-                                                type="tel"
-                                                name="phone"
-                                                id="phone"
-                                                placeholder="phone number"
-                                                required
-                                            />
-                                        </div>
-                                    </div> */}
-                                </div>
-
-                                {/* Plan duration  */}
-
-                                {/* <div>
-                                <div>Plan Duration</div>
-
-                                <div>
-                                    <div>
-                                        <input type="radio" id="oneMonth" name="month" value="oneMonth" />
-                                        <label for="oneMonth">1 month</label>
-                                    </div>
-
-                                    <div>
-                                        <input type="radio" id="threeMonth" name="month" value="threeMonth" />
-                                        <label for="threeMonth">3 month</label>
-                                    </div>
-
-                                    <div>
-                                        <input type="radio" id="sixMonth" name="month" value="sixMonth" />
-                                        <label for="sixMonth">6 month</label>
-                                    </div>
-
-                                    <div>
-                                        <input type="radio" id="twelveMonth" name="month" value="threeMonth" />
-                                        <label for="twelveMonth">12 month</label>
-                                    </div>
-                                </div>
-                            </div> */}
-
-                                {/* For the second div  */}
+                                <div></div>
 
                                 <div>
                                     {/* ====== Select Palyment Method  ========= */}
 
                                     <div>
                                         <div>
-                                            <label for="payment"> Select Payment Method</label>
+                                            <label htmlFor="payment"> Select Payment Method</label>
                                         </div>
 
                                         <div>
@@ -194,7 +123,7 @@ export default function Form() {
                                 </div>
                                 <div>
                                     <div>
-                                        <label for="question"> Remarks</label>
+                                        <label htmlFor="question"> Remarks</label>
                                     </div>
 
                                     <div>
@@ -202,8 +131,6 @@ export default function Form() {
                                     </div>
                                 </div>
                             </div>
-
-                            {/* ====== for right Section of the form ========= */}
 
                             {/* submit button  */}
 
--- src/components/PlanSubscribe/PlanSubscribe.jsx
+++ src/components/PlanSubscribe/PlanSubscribe.jsx
@@ -1,10 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import { Auth } from '../../allContext'
 import Form from './Form/Form'
 
 export default function PlanSubscribe() {
+    const [plans, setPlans] = useState([])
+    const [singlePlan, setSinglePlan] = useState({})
+    const [planId, setPlanId] = useState(0)
+
+    const { stateAuth } = useContext(Auth)
+    const token = stateAuth.token
+    const apiV1 = process.env.REACT_APP_API_V1
+
+    useEffect(() => {
+        let fectchData = async () => {
+            let response = await fetch(`${apiV1}/health-plan/`, {
+                headers: {
+                    Accept: 'appllication/json',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            let data = await response.json()
+
+            if (response.ok) {
+                setPlans(data)
+                console.log(data)
+            }
+        }
+        try {
+            fectchData()
+        } catch {
+            setPlans([])
+        }
+    }, [token, apiV1])
+
+    useEffect(() => {
+        let fectchData = async () => {
+            let response = await fetch(`${apiV1}/health-plan/${planId}`, {
+                headers: {
+                    Accept: 'appllication/json',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            let data = await response.json()
+
+            if (response.ok) {
+                setSinglePlan(data)
+                // important line
+                console.log('single', data)
+            }
+        }
+        try {
+            fectchData()
+        } catch {
+            setPlans([])
+        }
+    }, [token, apiV1, planId])
+
     return (
         <div>
-            <Form />
+            <Form plans={plans} singlePlan={singlePlan} setPlanId={setPlanId} />
         </div>
     )
 }
